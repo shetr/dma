8807005e46d9075884b638200968446f3549897c
Petr Šádek <sadekpet@fel.cvut.cz>
2021-07-13 15:07:57 +0200

    gcd and lcm finished

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
 
 use std::cmp::PartialEq;
+use std::cmp::PartialOrd;
 use std::ops::Rem;
+use std::ops::Mul;
+use std::ops::Div;
+
 use num_traits::Zero;
+use num_traits::OptAbs;
 
 /// Returns true if `a` divides `b`. Otherwise returns false.
 ///
@@ -34,12 +39,56 @@ pub fn is_divisible_by<T>(a: T, b: T) -> bool
     divides(b, a)
 }
 
-pub fn gdc(a: i32, b: i32) -> i32 {
-    0
+/// Computes greatest common divisor of `a` and `b`.
+/// 
+/// We define the greatest common divisor as the largest element of the set of common divisors if at least one of `a`, `b` is nonzero.
+/// Otherwise we define `gcd(0, 0) = 0`.
+pub fn gcd<T>(a: T, b: T) -> T 
+    where T: PartialOrd + Rem<Output = T> + Clone + Zero + OptAbs
+{
+    gcd_noabs(a.opt_abs(), b.opt_abs())
 }
 
-fn gdc_euclid(a: i32, b: i32) -> i32 {
-    0
+/// Computes least common multiple of `a` and `b`.
+/// 
+/// We define the least common multiple as the smallest element of the set of common multiples if both `a`, `b` are nonzero.
+/// Otherwise we define `lmc(a, 0) = lmc(0, b) = 0`.
+pub fn lcm<T>(mut a: T, mut b: T) -> T 
+    where T: PartialOrd + Rem<Output = T> + Mul<Output = T> + Div<Output = T> + Clone + Zero + OptAbs
+{
+    if a == T::zero() || b == T::zero() {
+        return T::zero();
+    }
+    a = a.opt_abs();
+    b = b.opt_abs();
+    (a.clone() * b.clone()) / gcd_noabs(a, b)
+}
+
+fn gcd_noabs<T>(a: T, b: T) -> T 
+    where T: PartialOrd + Rem<Output = T> + Clone + Zero
+{
+    match (a, b) {
+        (a, b) if a == T::zero() && b == T::zero() => T::zero(),
+        (a, b) if b == T::zero() => a,
+        (a, b) if a == T::zero() => b,
+        (a, b) if a > b => gcd_euclid(a, b),
+        (a, b) if a < b => gcd_euclid(b, a),
+        (a, _) => a
+    }
+}
+
+fn gcd_euclid<T>(mut a: T, mut b: T) -> T 
+    where T: PartialEq + Rem<Output = T> + Clone + Zero
+{
+    let mut b_clone = b.clone();
+    loop {
+        let r = a % b;
+        a = b_clone.clone();
+        b = r.clone();
+        b_clone = b.clone();
+        if b == T::zero() { break }
+    }
+    a
 }
 
 #[cfg(test)]
@@ -113,39 +162,104 @@ mod tests {
     }
 
 
-    fn test_gdc(a: i32, b: i32, res: i32) {
-        assert_eq!(gdc(a, b), res);
-        assert_eq!(gdc(b, a), res);
+    fn test_gcd(a: i32, b: i32, res: i32) {
+        assert_eq!(gcd(a, b), res);
+        assert_eq!(gcd(-a, b), res);
+        assert_eq!(gcd(a, -b), res);
+        assert_eq!(gcd(-a, -b), res);
+        assert_eq!(gcd(b, a), res);
+        assert_eq!(gcd(-b, a), res);
+        assert_eq!(gcd(b, -a), res);
+        assert_eq!(gcd(-b, -a), res);
     }
 
     #[test]
-    fn gdc_0() {
-        test_gdc(0, 0, 0);
+    fn gcd_0() {
+        test_gcd(0, 0, 0);
     }
     #[test]
-    fn gdc_1() {
-        test_gdc(1, 0, 1);
-        test_gdc(1, 1, 1);
-        test_gdc(1, 10, 1);
-        test_gdc(2, 3, 1);
-        test_gdc(2, 5, 1);
-        test_gdc(18, 19, 1);
+    fn gcd_1() {
+        test_gcd(1, 0, 1);
+        test_gcd(1, 1, 1);
+        test_gcd(1, 10, 1);
+        test_gcd(2, 3, 1);
+        test_gcd(2, 5, 1);
+        test_gcd(18, 19, 1);
     }
     #[test]
-    fn gdc_2() {
-        test_gdc(2, 6, 2);
-        test_gdc(6, 10, 2);
-        test_gdc(10, 14, 2);
+    fn gcd_2() {
+        test_gcd(2, 6, 2);
+        test_gcd(6, 10, 2);
+        test_gcd(10, 14, 2);
     }
     #[test]
-    fn gdc_3() {
-        test_gdc(3, 6, 3);
-        test_gdc(6, 9, 3);
-        test_gdc(9, 12, 3);
+    fn gcd_3() {
+        test_gcd(3, 6, 3);
+        test_gcd(6, 9, 3);
+        test_gcd(9, 12, 3);
     }
     #[test]
-    fn gdc_6() {
-        test_gdc(6, 12, 6);
-        test_gdc(12, 18, 6);
+    fn gcd_6() {
+        test_gcd(6, 12, 6);
+        test_gcd(12, 18, 6);
     }
+
+
+    fn test_lcm(a: i32, b: i32, res: i32) {
+        assert_eq!(lcm(a, b), res);
+        assert_eq!(lcm(-a, b), res);
+        assert_eq!(lcm(a, -b), res);
+        assert_eq!(lcm(-a, -b), res);
+        assert_eq!(lcm(b, a), res);
+        assert_eq!(lcm(-b, a), res);
+        assert_eq!(lcm(b, -a), res);
+        assert_eq!(lcm(-b, -a), res);
+    }
+
+    #[test]
+    fn lcm_0() {
+        test_lcm(0, 0, 0);
+        test_lcm(1, 0, 0);
+        test_lcm(2, 0, 0);
+        test_lcm(7, 0, 0);
+    }
+    #[test]
+    fn lcm_1() {
+        test_lcm(1, 1, 1);
+    }
+    #[test]
+    fn lcm_2() {
+        test_lcm(2, 2, 2);
+        test_lcm(1, 2, 2);
+    }
+    #[test]
+    fn lcm_3() {
+        test_lcm(3, 3, 3);
+        test_lcm(1, 3, 3);
+    }
+    #[test]
+    fn lcm_4() {
+        test_lcm(1, 4, 4);
+        test_lcm(2, 4, 4);
+        test_lcm(4, 4, 4);
+    }
+    #[test]
+    fn lcm_6() {
+        test_lcm(1, 6, 6);
+        test_lcm(2, 3, 6);
+        test_lcm(6, 6, 6);
+    }
+    #[test]
+    fn lcm_10() {
+        test_lcm(1, 10, 10);
+        test_lcm(2, 5, 10);
+        test_lcm(10, 10, 10);
+    }
+    #[test]
+    fn lcm_12() {
+        test_lcm(1, 12, 12);
+        test_lcm(3, 4, 12);
+        test_lcm(12, 12, 12);
+    }
+
 }
